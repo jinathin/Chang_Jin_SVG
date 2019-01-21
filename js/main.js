976427abcc5a237162357ed81fa0b378e97dd61a
Your Name <jinathin@gmail.com>
2019-01-21 13:52:47 -0500

    addJS

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(() =>{
+	// this is a self invoking anonymous function
+	console.log('fired!');
+
+	// these are the elements that you want to reference on the page (the icons in this case)
+	var badge = document.querySelector("img");
+
+	var otherBadges =document.querySelectorAll('.jpgBadge');
+
+	function logBadgeId() {
+		console.log(this.id);
+		debugger;
+	}
+
+	// what events are we listening for? clicks? mouse over and what should happen when we hear one
+	badge.addEventListener("click", logBadgeId);
+
+	//debugger;	
+
+	otherBadges.forEach(thisbadge => thisbadge.addEventListener("click", logBadgeId));
+
+	// this is the same as print in python
+})();
+
